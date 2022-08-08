1dc7472b4abc4fbbf5bb8453b43485f916b33bd3
Sergey <kuznetsov.s.i@bk.ru>
2022-08-08 11:46:42 +0300

    1. Quartz [#175122]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,7 +3,6 @@ package ru.job4j.quartz;
 
 import java.io.InputStream;
 import java.util.Properties;
-
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import static org.quartz.JobBuilder.*;
