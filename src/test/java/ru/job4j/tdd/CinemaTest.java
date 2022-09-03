9b62c86e422f7844c54f81db7e57f0a1fe059cd2
Sergey <kuznetsov.s.i@bk.ru>
2022-09-03 12:06:45 +0300

    2. Что такое TDD? [#4918]

---
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.tdd;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Calendar;
+import java.util.List;
+
+@Disabled
+public class CinemaTest {
+
+    @Test
+    public void whenBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket).isEqualTo(new Ticket3D());
+    }
+
+    @Test
+    public void whenFind() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions).isNull();
+    }
+
+    @Test
+    public void whenInvalidPlace() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        assertThrows(IllegalArgumentException.class, () -> {
+            cinema.buy(account, -1, 1, date);
+        });
+    }
+
+    @Test
+    public void whenPlaceAlreadyBuy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThrows(IllegalArgumentException.class, () -> {
+            cinema.buy(account, 1, 1, date);
+        });
+    }
+
+    @Test
+    public void whenDateIsOutDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.add(Calendar.DAY_OF_MONTH, -1);
+        assertThrows(IllegalArgumentException.class, () -> {
+            cinema.buy(account, 1, 1, date);
+        });
+    }
+}
