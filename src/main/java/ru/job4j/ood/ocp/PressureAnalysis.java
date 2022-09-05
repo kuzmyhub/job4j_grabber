7b1f034cd927c9052063c8282973a571024c1239
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-09-05 15:56:08 +0300

    0. Принцип открытости закрытости [#4914 #339686]

--- src/main/java/ru/job4j/ood/ocp/PressureAnalysis.java
+++ src/main/java/ru/job4j/ood/ocp/PressureAnalysis.java
@@ -1,8 +1,11 @@
 package ru.job4j.ood.ocp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PressureAnalysis {
 
-    public int analyse(int yesterdayPressure, int todayPressure) {
-        return yesterdayPressure - todayPressure;
+    public int analyse(List<Meteo> list) {
+        return list.stream().mapToInt(x -> x.getPressure()).sum();
     }
 }
