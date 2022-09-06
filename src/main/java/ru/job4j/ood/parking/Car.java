c207a56f07c6a2d687fec1b3602a7f5d555d6cc3
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-09-06 10:00:02 +0300

    2. Парковка машин [#853]

---
+++ src/main/java/ru/job4j/ood/parking/Car.java
@@ -0,0 +1,5 @@
+package ru.job4j.ood.parking;
+
+public interface Car {
+    void getSize();
+}
---
+++ src/main/java/ru/job4j/ood/parking/ParkingSpace.java
@@ -0,0 +1,5 @@
+package ru.job4j.ood.parking;
+
+public interface ParkingSpace {
+    void park(Car car);
+}
