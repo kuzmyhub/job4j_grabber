e2a238fa67d2d6a9d4dced34d1505e1dc9c1b6d1
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-09-06 11:30:21 +0300

    0. Принцип разделения интерфейсов [#4916]

---
+++ src/main/java/ru/job4j/ood/isp/Fridge.java
@@ -0,0 +1,9 @@
+package ru.job4j.ood.isp;
+
+public interface Fridge {
+    void makeIce();
+
+    void coolFood();
+
+    void freezeFood();
+}
---
+++ src/main/java/ru/job4j/ood/isp/Teacher.java
@@ -0,0 +1,9 @@
+package ru.job4j.ood.isp;
+
+public interface Teacher {
+    void conductLessons();
+
+    void followDiscipline();
+
+    void monitorTeachers();
+}
